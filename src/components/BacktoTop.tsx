00aadf840ebb848a1e8ecdc75416f8334c175c6b
AirZED <regnalukpabio@gmail.com>
2023-05-20 07:44:38 +0100

    basic:push for test

--- src/components/BacktoTop.tsx
+++ src/components/BacktoTop.tsx
@@ -5,8 +5,18 @@ import { IoIosArrowUp } from "react-icons/io";
 import style from "./../styles/BacktopTop.module.css";
 
 const BacktoTop = (): JSX.Element => {
+  const scrolltoTopHandler = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className={style.backtotop} title="Back to top">
+    <div
+      className={style.backtotop}
+      title="Back to top"
+      onClick={scrolltoTopHandler}
+    >
       <IoIosArrowUp />
     </div>
   );
