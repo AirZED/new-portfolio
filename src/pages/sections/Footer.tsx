2be5cabcdd09e6fba45bd7cf9a7707af0f730c93
AirZED <regnalukpabio@gmail.com>
2023-06-04 22:55:49 +0100

    fixed footer

--- src/pages/sections/Footer.tsx
+++ src/pages/sections/Footer.tsx
@@ -1,9 +1,13 @@
 import style from "./../../styles/Footer.module.css";
 
+// importing icons
+import { FaSmileWink } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className={style.footer}>
       <p>Designed and Built with Passion </p>
+      <FaSmileWink className={style.icon} />
     </div>
   );
 };
