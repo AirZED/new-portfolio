521a0e29266718a52d1c44e01d9bb78cfe67825c
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2024-10-16 21:21:21 +0100

    update stack and image

--- src/pages/sections/AboutMe.tsx
+++ src/pages/sections/AboutMe.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 import style from "./../../styles/AboutMe.module.css";
 
 // importing image
-import displayImage from "./../../assets/images/me.png";
+import displayImage from "./../../assets/images/me.jpg";
 
 // importing supporting component
 import Topic from "../../components/Topic";
@@ -15,20 +15,20 @@ const AboutMe = forwardRef((props, ref: any) => {
         <Topic content="About Mfoniso" />
       </div>
       <div className={style.textArea}>
+        <img src={displayImage} alt="mfoniso standing and smiling" />
         <div className={style.text}>
           <p>
             <span>Mfoniso Ukpabio</span> is a highly skilled and detail-oriented
-            MERN stack developer, specializing in TypeScript, Next.js, React,
-            and Node.js. With a strong background in problem-solving, Data
-            Structures, and Algorithms (DSA), Mfoniso brings a deep
-            understanding of efficient code implementation and optimization
-            strategies. Their dedication to delivering high-quality and scalable
-            solutions is evident through their successful completion of multiple
-            internships and ongoing studies in Computer Engineering at the
-            University of Uyo.
+            Software Engineer, specializing in Rust, ExpressJs, TypeScript,
+            Next.js, React, and Node.js. With a strong background in
+            problem-solving, Data Structures, and Algorithms (DSA), Mfoniso
+            brings a deep understanding of efficient code implementation and
+            optimization strategies. Their dedication to delivering high-quality
+            and scalable solutions is evident through their successful
+            completion of multiple internships and ongoing studies in Computer
+            Engineering at the University of Uyo.
           </p>
         </div>
-        <img src={displayImage} alt="mfoniso standing and smiling" />
       </div>
     </div>
   );
